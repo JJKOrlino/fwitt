1fb6178adca729a91de7b54832f7c4cbd0a74c9f
JJKOrlino <jjkorlino80@yahoo.com>
2021-01-08 23:38:51 -0600

    journal entry controller complete

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -1,28 +1,102 @@
 class JournalEntriesController < ApplicationController
-    #render form for new entries
-    get '/journal_entries_new' do
-        erb :'/journal_entries/new'
-    end
 
-    #post entries
+    get '/journal_entries' do
+      @journal_entries = JournalEntry.all
+      erb :'journal_entries/index'
+    end
+  
+    get '/journal_entries/new' do
+      redirect_if_not_logged_in
+      erb :'/journal_entries/new'
+    end
+  
     post '/journal_entries' do
-        if !logged_in?
-            redirect '/'
-        end
-
-        if params[:content] != ""
-            @journal_entries = JournalEntry.create(content: params[:content], user_id: current_user.id)
-            redirect "/journal_entries/#{journal_entry.id}"
-        else
-            redirect '/journal_entries/new'
-        end
+      redirect_if_not_logged_in
+      if params[:content] != ""
+        @journal_entry = JournalEntry.create(content: params[:content], user_id: current_user.id, title: params[:title], mood: params[:mood])
+        flash[:message] = "Journal entry successfully created." if @journal_entry.id
+        redirect "/journal_entries/#{@journal_entry.id}"
+      else
+        flash[:errors] = "Something went wrong - you must provide content for your entry."
+        redirect '/journal_entries/new'
+      end
     end
-
-    #show page for entries
-    get '/journal_entries/:id'
-        @journal_entry = JournalEntry.find(params[:id])
-        erb :'/journal_entries/show'
+  
+    get '/journal_entries/:id' do
+      set_journal_entry
+      erb :'/journal_entries/show'
     end
+  
+    
+    get '/journal_entries/:id/edit' do
+      redirect_if_not_logged_in
+      set_journal_entry
+      if authorized_to_edit?(@journal_entry)
+        erb :'/journal_entries/edit'
+      else
+        redirect "users/#{current_user.id}"
+      end
+    end
+  
+    patch '/journal_entries/:id' do
+      redirect_if_not_logged_in
+      set_journal_entry
+      if @journal_entry.user == current_user && params[:content] != ""
+        @journal_entry.update(content: params[:content])
+        redirect "/journal_entries/#{@journal_entry.id}"
+      else
+        redirect "users/#{current_user.id}"
+      end
+    end
+  
+    delete '/journal_entries/:id' do
+      set_journal_entry
+      if authorized_to_edit?(@journal_entry)
+        @journal_entry.destroy
+        flash[:message] = "Successfully deleted that entry."
+        redirect '/journal_entries'
+      else
+        redirect '/journal_entries'
+      end
+    end
+  
+    private
+  
+    def set_journal_entry
+      @journal_entry = JournalEntry.find(params[:id])
+    end
+  end
 
-    #index route for all entries
-end
+# class JournalEntriesController < ApplicationController
+
+    #     get '/journal_entries' do
+    #         @journal_entries = JournalEntry.all
+    #         erb :'journal_entries/index'
+    #     end
+    
+    #     get '/journal_entries_new' do
+    #         redirect_if_not_logged_in
+    #         erb :'/journal_entries/new'
+    #     end
+    
+    #     #post entries
+    #     post '/journal_entries' do
+    #         if !logged_in?
+    #             redirect '/'
+    #         end
+    
+    #         if params[:content] != ""
+    #             @journal_entry = JournalEntry.create(content: params[:content], user_id: current_user.id)
+    #             redirect "/journal_entries/#{@journal_entry.id}"
+    #         else
+    #             redirect '/journal_entries/new'
+    #         end
+    #     end
+    
+    #     #show page for entries
+    
+    #     get '/journal_entries/:id' do
+    #         set_journal_entry
+    #         erb :'/journal_entries/show'
+    #     end
+    # end
