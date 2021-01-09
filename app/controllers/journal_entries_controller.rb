1f0e9dae2ebec5e972f9e787bbe2adc87c302901
JJKOrlino <jjkorlino80@yahoo.com>
2021-01-08 22:44:57 -0600

    journal entries controller + entries

---
+++ app/controllers/journal_entries_controller.rb
@@ -0,0 +1,12 @@
+class JournalEntriesController < ApplicationController
+    #render form for new entries
+    get '/journal_entries_new' do
+        erb :'/journal_entries/new'
+    end
+
+    #post entries
+
+    #show page for entries
+
+    #index route for all entries
+end
