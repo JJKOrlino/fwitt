54eb36aa9a251d5bb0842eb78300d570f1962e46
JJKOrlino <jjkorlino80@yahoo.com>
2021-01-08 21:27:17 -0600

    session and show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,24 @@ class UsersController < ApplicationController
         erb :login
     end
 
+    post '/login' do
+        @user = User.find_by(email: params[:email])
+        
+        if @user.authenticate(params[:password])
+            session[:user_id] = @user.id 
+            puts session
+            redirect "users/#{@user.id}"
+        else 
+
+        end
+    end
+
     get '/signup' do
 
+    end
+
+    get '/users/:id' do
+        "user show page"
     end
 
 end
