963884deb6cd66a35497ee1b7f35ee122624e7d4
JJKOrlino <jjkorlino80@yahoo.com>
2021-01-08 22:57:59 -0600

    journal entries

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -5,7 +5,18 @@ class JournalEntriesController < ApplicationController
     end
 
     #post entries
+    post '/journal_entries' do
+        if !logged_in?
+            redirect '/'
+        end
 
+        if params[:content] != ""
+            @journal_entries = JournalEntry.create(content: params[:content], user_id: current_user.id)
+            redirect "/journal_entries/#{journal_entry.id}"
+        else
+            redirect '/journal_entries/new'
+        end
+    end
     #show page for entries
 
     #index route for all entries
